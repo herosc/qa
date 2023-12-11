d0b27cda4960d97ddbe07e6ecc78ce41546ccd3a
Heros Carvalho <heroscarvalho.neto@gmail.com>
2023-12-11 09:49:33 -0300

    Eita

--- features/step_definitions/movies_steps.rb
+++ features/step_definitions/movies_steps.rb
@@ -1,7 +1,7 @@
 Given('i search by title in a GET request:') do |table|
   @params = table.rows_hash
   headers = {
-    "X-RapidAPI-Key" => '7d9e259944msh2e8869368b5dd68p177be8jsn6d0a33ead33f',
+    "X-RapidAPI-Key" => '',
     "X-RapidAPI-Host" => 'movie-database-alternative.p.rapidapi.com'
   }
   @response = HTTParty.get('https://movie-database-alternative.p.rapidapi.com/', query: {
